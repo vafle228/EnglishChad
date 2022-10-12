c92c22ebf81519d523e1e9b6d512e918dd7ab590
NeiroFucker <60180871+NeiroFucker@users.noreply.github.com>
2022-10-12 23:38:15 +0500

    Back to normal 100%

--- ChadUtils/constants.py
+++ ChadUtils/constants.py
@@ -6,3 +6,8 @@ REGION_NAME = "eu-central-1"
 # Telegram constants
 API_KEY = "5706248141:AAHpdzAaHLeYia2VFFh420Rog2dzPqt08rM"
 API_REQUEST = f"https://api.telegram.org/file/bot{API_KEY}/"
+
+# Bot constants
+TEMP_ROOT = "tmp/{0}"
+MAX_FILE_SIZE = 20 * 1024 * 1024  # 20MB
+ENTRY_PER_PAGE = 5
