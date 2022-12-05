6be85d837fe8eb6b0855454e3d297170ad2d6afd
NeiroFucker <60180871+NeiroFucker@users.noreply.github.com>
2022-12-05 18:18:02 +0500

    init

--- main.py
+++ main.py
@@ -1,18 +1,58 @@
+import os
+from io import BufferedReader
+
 import telebot
-from flask import Flask, request
+from telebot.types import Message
 
-from ChadUtils.constants import SERVER_HOOK
-from telegrambot import bot
+from ChadLogic.englishchad import EnglishChadBot
+from ChadUtils.constants import API_KEY
+from MessageStructs.telegramstruct import TelegramMessage
+
+bot = telebot.TeleBot(API_KEY)
 
 
-bot.remove_webhook(); bot.set_webhook(SERVER_HOOK)
-
-app = Flask(__name__)
-
-@app.route("/", methods=["POST"])
-def englishChadWebhook():
-    update_json = request.stream.read().decode("utf-8")
-    telegram_update = telebot.types.Update.de_json(update_json)
-    bot.process_new_updates([telegram_update])
+@bot.message_handler(commands=[
+    "start", "help", "addsolution", 
+    "getsolution", "delsolution", "showsolution", 
+    "subscribe", "unsubscribe"
+])
+def addNewEntry(message: Message):
+    command = message.text.split(" ")[0]
+    tele_messsage = TelegramMessage(message, bot)
     
-    return ("Ok", 200)
+    EnglishChadBot.startUserHandling(tele_messsage, command)
+    bot.send_message(message.chat.id, EnglishChadBot.handleUserMessage(tele_messsage))
+
+
+@bot.message_handler(content_types=["document", "text"])
+def replyToMessage(message: Message):
+    message = removeErrorSymbols(message)
+    tele_messsage = TelegramMessage(message, bot)
+    result = EnglishChadBot.handleUserMessage(tele_messsage)
+
+    if not isinstance(result, BufferedReader):
+        return bot.send_message(message.chat.id, result)
+    
+    bot.send_document(message.chat.id, result)
+    result.close(); os.remove(result.name)
+
+
+def removeErrorSymbols(message: Message):
+    if message.text is not None:
+        message.text = message.text.replace("'", "")
+    if message.from_user.username is not None:
+        message.from_user.username = message.from_user.username.replace("'", "")
+    return message
+
+
+def bot_polling():
+    while "Bot polling loop":
+        try:
+            print("New bot instance started")
+            bot.polling(none_stop=True, interval=0)
+        except Exception as e:
+            print(f"Bot failed with \n{e}")
+            bot.stop_polling()
+
+
+if __name__ == "__main__": bot_polling()
